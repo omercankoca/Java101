9113ce6491df9cd8bd145a55480e9b6629c6d8ed
Ömer Can Koca <omer.c.koca@gmail>
2024-07-11 13:35:53 +0300

    Harmonic Numbers programı düzenlendi.

--- HarmonicNumbers.java
+++ HarmonicNumbers.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class HarmonicNumbers {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter the N number : ");
         int i = input.nextInt();
         double result = 0.0;
         for (int n = 1 ; n<=i; n++){
