b87a63663795fde94f9b14dda0c847e4e5afa253
Ömer Can Koca <omer.c.koca@gmail>
2024-08-05 13:51:33 +0300

    ArrayFrequent eklendi.

---
+++ ArrayFrequent.java
@@ -0,0 +1,36 @@
+public class ArrayFrequent {
+    static boolean isFind(int[] arr, int value){
+        for (int i : arr){
+            if (i == value){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] list = {10, 20, 20, 10, 10, 20, 5, 20};
+        int[] checkedList = new int[list.length];
+        int checkIndex = 0;
+
+        for (int i = 0; i < list.length; i++) {
+
+            if (!isFind(checkedList, list[i])){
+                int sum = 0;
+                int frequent = 0;
+                for (int j = 0; j < list.length; j++) {
+                    if (list[i] == list[j]) {
+                        sum += list[i];
+                        frequent = sum / list[i];
+
+
+                    }
+                }
+                checkedList[checkIndex++] = list[i];
+                System.out.println(list[i] + " sayısı " + frequent + " kere tekrar etti.");
+            }
+
+
+        }
+    }
+}
